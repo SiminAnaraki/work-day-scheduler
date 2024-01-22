36e06b0a4296b3b5169a75aa2ff83d244595d84b
SiminAnaraki <simin.anaraki@gmail.com>
2024-01-22 20:50:08 +0000

    working with dayjs()

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ function suffixe(d) {
     }
   };
 var time = dayjs().format("dddd, MMMM DD")
+
+var hour = dayjs().format("H")
+console.log(hour)
+for(var i=9 ;i<18;i++){
+    var saj = $(`#${i}`).attr("id")
+    console.log(saj)
+   if(saj===hour){$(`#${i}`).addClass("present")}
+    else if(saj<hour){$(`#${i}`).addClass("past")}
+    else {$(`#${i}`).addClass("future")}
+
+   }
+
 $("#currentDay").text(time +suffixe(today));
 $("#9").val(localStorage.getItem("9"))
 $("#10").val(localStorage.getItem("10"))
@@ -19,6 +31,8 @@ $("#14").val(localStorage.getItem("14"))
 $("#15").val(localStorage.getItem("15"))
 $("#16").val(localStorage.getItem("16"))
 $("#17").val(localStorage.getItem("17"))
+
+
 
 
 
